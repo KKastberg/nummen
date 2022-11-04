43cc334211c7cb9b94ed3472d54991cf30e32340
kevin <kevin@kastberg.se>
2022-11-04 16:11:00 +0100

    Completed problem 1

--- cmd/lab1/problem1.go
+++ cmd/lab1/problem1.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/plotutil"
+	"gonum.org/v1/plot/vg"
+)
+
+const v_max = 25;		// m/s
+const d = 75; 			// m
+
+func f(x float64) float64 {
+	if x <= 0 {
+		return 0
+	} else if x < d {
+		return x * v_max / d
+	}
+	return v_max
+}
+
+func main() {
+	p := plot.New()
+	
+	p.Title.Text = "Graph of f(x)"
+	p.X.Label.Text = "x"
+	p.Y.Label.Text = "y"
+
+	pts := make(plotter.XYs, 100)
+	for i := - 10; i < pts.Len() - 10; i++ {
+		pts[i+10].X = float64(i)
+		pts[i+10].Y = float64(f(float64(i)))
+	}
+
+	plotutil.AddLinePoints(p, "f(x)", pts)
+
+	// Save the plot to a PNG file.
+	if err := p.Save(4*vg.Inch, 4*vg.Inch, "problem1.png"); err != nil {
+		panic(err)
+	}
+}
--- cmd/lab1/scratch1.go
+++ cmd/lab1/scratch1.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"gonum.org/v1/gonum/mat"
 )
 
 func main() {
