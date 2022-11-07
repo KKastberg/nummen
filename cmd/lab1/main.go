ce8939f5ad4f6e55a5bf657222bef41b8a9e107a
kevin <kevin@kastberg.se>
2022-11-07 11:51:06 +0100

    Problem 2 solved

--- cmd/lab1/main.go
+++ cmd/lab1/main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
+
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/plotutil"
 	"gonum.org/v1/plot/vg"
 )
 
-const v_max = 25;		// m/s
-const d = 75; 			// m
+const v_max float64 = 25;		// Max speed [m/s]
+const d float64 = 75; 			// Max distance between cars [m]
+const M int = 10				    // Count of cars [cars]
+const h float64 = 0.5				// Step size [step]
+const t float64 = 40;		    // Time instant [s]
+const g float64 = 5.0;		  // Speed of first car [m/s]
+const di float64 = d;		    // Initial distances between cars [m]
 
 func f(x float64) float64 {
 	if x <= 0 {
@@ -43,11 +49,51 @@ func p1() {
 }
 
 func p2() {
+	const n int = int(t/h)  // Count of steps
+
+	// Generate list of initial car positions
+	carPos := make([]float64, M)
+	for i := 1; i <= M; i++ {			
+		carPos[i - 1] = float64(i) * di
+	}
+	fmt.Println(carPos)
+	for i := 0; i < n; i++ {
+		nextCarPos, carSpeed := euler_step(carPos)
+		fmt.Println(carSpeed)
+		carPos = nextCarPos
+	}
 
 }
 
+func get_car_speed(carPos float64, nextCarPos float64) float64 {
+	return f(nextCarPos - carPos)		// Calculate speed of car i
+}
+
+func get_all_car_speeds(carPos []float64) []float64 {
+	carSpeed := make([]float64, M)		// List of car speeds
+	for i := 0; i < M - 1; i++ {
+		carSpeed[i] = get_car_speed(carPos[i], carPos[i + 1]) 	// Calculate speed of car i
+	}
+	carSpeed[M - 1] = g		// Speed of car M is constant g
+	return carSpeed
+}
+
+func get_new_car_pos(carPos []float64, carSpeed []float64) []float64 {
+	newCarPos := make([]float64, M)		// List of car positions
+	for i := 0; i < M; i++ {
+		newCarPos[i] = carPos[i] + carSpeed[i] * h		
+	}
+	return newCarPos
+}
+
+func euler_step(carPos []float64) ([]float64, []float64) {
+	carSpeed := get_all_car_speeds(carPos)
+	newCarPos := get_new_car_pos(carPos, carSpeed)
+	return newCarPos, carSpeed
+}
+
 func run_all() {
-	fmt.Println("Running problem 1"); p1()
+	// fmt.Println("Running problem 1"); p1()
 	fmt.Println("Running problem 2"); p2()
 }
 
