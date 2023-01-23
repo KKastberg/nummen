3585c55006496fff954ba9b57a72862a587fa385
babaksamimi <babak@samimi.se>
2023-01-23 13:08:09 +0100

    WIP of polynomial interpolation

--- cmd/lab2/main.go
+++ cmd/lab2/main.go
@@ -8,90 +8,106 @@ import (
 )
 
 func print_matrix(matrix *mat.Dense) {
-	var rows, _ = matrix.Dims()
-func p3() {
-	x := mat.NewDense(6, 1, []float64{150, 200, 300, 500, 1000, 2000});
-	y := mat.NewDense(6, 1, []float64{2, 3, 4, 5, 6, 7});
-
-	p3a5(x, y);
-}
-
-func p3a5(x *mat.Dense, y *mat.Dense) {
-	// 1/U(x) - 1/8 = a/x
-	A := mat.NewDense(6,1, nil)
-	for i := 0; i < 6; i++ {
-		A.Set(i, 0, 1/x.At(i,0));
-	}
-
-	b := mat.NewDense(6, 1, nil);
-	for i := 0; i < 6; i++ {
-		b.Set(i, 0, 1/y.At(i,0) - 1.0/8.0);
-	}
-
-	least_square(A, b);
-}
-
+	var rows, columns = matrix.Dims()
 	for i := 0; i < rows; i++ {
-		for j := 0; j < rows+1; j++ {
+		for j := 0; j < columns; j++ {
 			fmt.Printf("%f ", matrix.At(i, j))
 		}
 		fmt.Println("\n")
 	}
-
 }
+
+func print_vector(vector mat.VecDense) {
+	var rows, _ = vector.Dims()
+
+	for i := 0; i < rows; i++ {
+		fmt.Printf("%f ", vector.At(i, 0))
+
+	}
+	fmt.Println("\n")
+}
+
+func p3() {
+	x := mat.NewDense(6, 1, []float64{150, 200, 300, 500, 1000, 2000})
+	y := mat.NewDense(6, 1, []float64{2, 3, 4, 5, 6, 7})
+
+	p3a5(x, y)
+}
+
+func p3a5(x *mat.Dense, y *mat.Dense) {
+	// 1/U(x) - 1/8 = a/x
+	A := mat.NewDense(6, 1, nil)
+	for i := 0; i < 6; i++ {
+		A.Set(i, 0, 1/x.At(i, 0))
+	}
+
+	b := mat.NewDense(6, 1, nil)
+	for i := 0; i < 6; i++ {
+		b.Set(i, 0, 1/y.At(i, 0)-1.0/8.0)
+	}
+
+	least_square(A, b)
+}
+
 func p3a6(x *mat.Dense, y *mat.Dense) {
 	// 1/U(x) - 1/8 = a/x
-	A := mat.NewDense(6,1, nil)
+	A := mat.NewDense(6, 1, nil)
 	for i := 0; i < 6; i++ {
-		A.Set(i, 0, 1/x.At(i,0));
+		A.Set(i, 0, 1/x.At(i, 0))
 	}
 
-	b := mat.NewDense(6, 1, nil);
+	b := mat.NewDense(6, 1, nil)
 	for i := 0; i < 6; i++ {
-		b.Set(i, 0, 1/y.At(i,0) - 1.0/8.0);
+		b.Set(i, 0, 1/y.At(i, 0)-1.0/8.0)
 	}
 
-	least_square(A, b);
+	least_square(A, b)
 }
 
 func p2() {
-	a := mat.NewDense(7, 1, []float64{0, 0.5, 1, 1.5, 2, 2.99, 3});
-	b := mat.NewDense(7, 1, []float64{0, 0.52, 1.09, 1.75, 2.45, 3.5, 4});
-	
+	a := mat.NewDense(7, 1, []float64{0, 0.5, 1, 1.5, 2, 2.99, 3})
+	b := mat.NewDense(7, 1, []float64{0, 0.52, 1.09, 1.75, 2.45, 3.5, 4})
+
 	// Least square
-	A := mat.NewDense(7,2, nil)
+	A := mat.NewDense(7, 2, nil)
 	for i := 0; i < 7; i++ {
-		A.Set(i, 0, a.At(i,0));
-		A.Set(i, 1, a.At(i,0)*a.At(i,0));
+		A.Set(i, 0, a.At(i, 0))
+		A.Set(i, 1, a.At(i, 0)*a.At(i, 0))
 	}
-	least_square(A, b);
+	least_square(A, b)
 
 	// polynomial interpolation
-	polynomial_interpolation(a, b);
+	polynomial_interpolation(a, b)
 
 }
 
 func polynomial_interpolation(a *mat.Dense, b *mat.Dense) {
 	var rows, _ = a.Dims()
-	vander_matrix := mat.NewDense(rows, rows+1, nil)
+	vander_matrix := mat.NewDense(rows, rows, nil)
 
 	for row := 0; row < rows; row++ {
 		//A.Set(row, 0, 1) // First column in every row needs to be equal to 1
-		for col := 0; col < rows+1; col++ {
+		for col := 0; col < rows; col++ {
 			vander_matrix.Set(row, col, math.Pow(a.At(row, 0), float64(col)))
 		}
 	}
 
+	fmt.Printf("Vander matrix dimensions: %dx%d\n", rows, rows)
 	print_matrix(vander_matrix)
 
-	 := mat.NewDense(rows+1, 1, nil)
+	y := mat.NewVecDense(rows, nil)
+	_ = y.CopyVec(b.ColView(0))
 
+	var c mat.VecDense
+
+	c.SolveVec(vander_matrix, y)
+	print_vector(c)
 
 }
 
 func least_square(A *mat.Dense, b *mat.Dense) *mat.Dense {
-	_, A_cols := A.Dims();
-	_, b_cols := b.Dims();
+	_, A_cols := A.Dims()
+	_, b_cols := b.Dims()
 
 	// Create the matrix (A^T*A)^-1
 	A_T_A := mat.NewDense(A_cols, A_cols, nil)
@@ -114,11 +130,10 @@ func least_square(A *mat.Dense, b *mat.Dense) *mat.Dense {
 	fmt.Printf("b: %v\n", b)
 	fmt.Printf("x: %v\n", x)
 
-	return x;
+	return x
 }
 
 // Entry point
 func main() {
 	p2()
 }
-
--- go.mod
+++ go.mod
@@ -8,12 +8,21 @@ require (
 )
 
 require (
+	github.com/gonum/blas v0.0.0-20181208220705-f22b278b28ac // indirect
+	github.com/gonum/floats v0.0.0-20181209220543-c233463c7e82 // indirect
+	github.com/gonum/internal v0.0.0-20181124074243-f884aa714029 // indirect
+	github.com/gonum/lapack v0.0.0-20181123203213-e4cdc5a0bff9 // indirect
+)
+
+require (
 	git.sr.ht/~sbinet/gg v0.3.1 // indirect
 	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
 	github.com/go-fonts/liberation v0.2.0 // indirect
 	github.com/go-latex/latex v0.0.0-20210823091927-c0d11ff05a81 // indirect
 	github.com/go-pdf/fpdf v0.6.0 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/gonum/matrix v0.0.0-20181209220409-c518dec07be9
 	golang.org/x/image v0.0.0-20220902085622-e7cb96979f69 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	gonum.org/v1/exp v0.0.0-20220503065741-e45430a0816d
 )
